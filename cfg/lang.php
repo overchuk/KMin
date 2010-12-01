104d17eaaf6d0d2589315553288162f4912de017
Iliya Liberman <iliya@4-d.su>
2010-12-01 14:21:22 +0300

    Adedd KMlang parameters, for DB mask

--- cfg/lang.php
+++ cfg/lang.php
@@ -1,6 +1,6 @@
 <?
 	// Русский язык
-
-	KMlang::$langs = array('ru', 'en', 'ge');
+	KMlang::$langs      = array('ru', 'en', 'ge');
 	KMlang::$lang_names = array('рус', 'eng', 'ger');
+	KMlang::$mask       = array('R', 'E', 'G');
 ?>
