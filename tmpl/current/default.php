dece5b326a6c04e207263924022945fd2e65ae72
Peter Volsky <peter@4-d.su>
2011-01-06 20:43:02 +0300

    Fixed default template

--- tmpl/current/default.php
+++ tmpl/current/default.php
@@ -2,14 +2,13 @@
 	KM::ns('page');
 	KM::ns('html');
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title><?=KMpage::title();?></title>
 <meta name="keywords" content="<?=KMpage::keywords();?>">
 <meta name="description" content="<?=KMpage::description();?>">
-<link href="style.css" rel="stylesheet" type="text/css">
 <?=KMhtml::head();?>
 </head>
 <?=$body;?>
