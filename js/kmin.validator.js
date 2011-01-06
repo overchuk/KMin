57aae7a32b89ddbbc28ef2c968beb554e3cdffd3
Peter Volsky <peter@4-d.su>
2011-01-06 20:45:51 +0300

    Fixed javascript validators for nembers, strings

--- js/kmin.validator.js
+++ js/kmin.validator.js
@@ -1,29 +1,47 @@
 if(!kmin)var kmin=new Object();
 if(!kmin.validator)kmin.validator=new Object();
 
-kmin.validator.integer=function(id,min,max){
+kmin.validator.num=function(id,min,max){
     var i = document.getElementById(id).value * 1.0;
-    return !((i < min) || (i > max));
+	if(min != '')
+	{
+		min = min*1.0;
+		if(i < min)
+			return false;
+	}
+
+	if(max != '')
+	{
+		max = max*1.0;
+		if(i > max)
+			return false;
+	}
+
+    return true;
 }
 
-
-kmin.validator.mask=function(id, mask){
-    var s = new String( document.getElementById(id).value );
-    var r = new RegExp(mask);
-    return s.match(r);
-}
-
-kmin.validator.str=function(id,min,max,mask){
-    
+kmin.validator.str=function(id, min, max, mask){
     var s = new String( document.getElementById(id).value );
     var l = s.length;
 
-    if(min && (l < min))
-        return false;
+	if(min != '')
+	{
+		min = min*1.0;
+		if(l < min)
+			return false;
+	}
 
-    if(max && (l > max))
-        return false;
+	if(max != '')
+	{
+		max = max*1.0;
+		if(l > max)
+			return false;
+	}
 
+	if(mask == '')
+		return true;
+
+    var r = new RegExp(mask);
+    return r.test(s);
 }
-
 
