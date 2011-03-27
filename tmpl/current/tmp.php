95f607fdff97ca9e54c6a06ca0f43465cc9ac6c0
unknown <olik@.(none)>
2011-03-27 23:19:15 +0400

    Added utf8 header

--- tmpl/current/tmp.php
+++ tmpl/current/tmp.php
@@ -1,6 +1,7 @@
 <?
     KM::ns('tmpl');
     KM::ns('html');
+	header("content-type: text/html; charset=utf-8");
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
 "http://www.w3.org/TR/html4/loose.dtd">
